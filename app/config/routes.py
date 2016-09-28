55dc6fd0689d5cdd113f185938513daccb1f4ec6
Harvey Chui <chuiharvey@gmail.com>
2016-09-28 12:44:08 -0700

    Re-added the Routes file

---
+++ app/config/routes.py
@@ -0,0 +1,33 @@
+"""
+    Routes Configuration File
+
+    Put Routing rules here
+"""
+from system.core.router import routes
+
+routes['default_controller'] = 'Main#index'
+routes['GET']['/register'] = 'Main#register'
+routes['GET']['/login'] = 'Main#login'
+routes['POST']['/process'] = 'Main#process'
+routes['GET']['/logout'] = 'Main#logout'
+
+
+routes['GET']['/home'] = 'Profile#home'
+routes['GET']['/calander'] = 'Profile#calender'
+routes['GET']['/contact'] = 'Profile#contact'
+routes['GET']['/find'] = 'Profile#find'
+routes['GET']['/pupil'] = 'Profile#pupil'
+
+
+routes['GET']['/tutor'] = 'appointment#tutor'
+routes['GET']['/tutor/<user_id>'] = 'appointment#showprofile'
+routes['GET']['/tutor/order'] = 'appointment#acceptFriend'
+routes['GET']['/chat'] = 'appointment#chat'
+routes['GET']['/tutor/<tutor_id>/review'] = 'appointment#review'
+routes['GET']['/tutor/<tutor_id>/calender'] = 'appointment#calender'
+routes['GET']['/tutor/<tutor_id>/review/add'] = 'appointment#add_review'
+routes['GET']['/tutor/<tutor_id>/review/post'] = 'appointment#post'
+routes['GET']['/tutor/<tutor_id>/hire'] = 'appointment#hire'
+routes['GET']['/tutor/<tutor_id>/map'] = 'appointment#map'
+routes['GET']['/tutor/<tutor_id>/order'] = 'appointment#order'
+routes['GET']['/tutor/<tutor_id>/appoint'] = 'appointment#appoint'
